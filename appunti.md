f58260858b10f4e033eebc66b5039ae2a7a65a22
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-10-20 15:50:31 +0200

    Create layout grid

---
+++ appunti.md
@@ -0,0 +1,13 @@
+creo una funzione  numerocelle che prende come parametro il livello della difficoltà ovvero il valore che inserisce l'utente con il form. Con questa funzione imposto il numero massimo di celle.
+Questa funzione viene richiamata al click del bottone
+Avendo determinato il numero delle celle da rappresentare, facendo la radice quadrata mi calcolo il numero delle colonne
+
+creo un ciclo che va da o al numero di celle da aggiungere, ad ongi iterazione questo ciclo mi aggiunge un elemento al game.container
+    creo l'elemento cell 
+        ho creato un elemento div 
+        a cui ho aggiunto la classe cel-box-con il numero delle colonne
+        e dopo ho aggiunto all'html con il metodo append l'elemento.
+
+
+la classe cell-box-numberCol le ho sintetizzate così 
+
---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+/*
+
+L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata,
+    in cui ogni cella contiene un numero tra quelli compresi in un range:
+        con difficoltà 1 => tra 1 e 100
+        con difficoltà 2 => tra 1 e 81
+        con difficoltà 3 => tra 1 e 49
+
+    Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+
+*/
+
+const btnSubmit = document.getElementById("btn_start_game")
+const btnLevel = document.getElementById("levelup")
+const gameContainer = document.getElementById("boxgame")
+
+btnSubmit.addEventListener("click", function () {
+    const difficoltLevel = btnLevel.value
+    const numberCellToAdd = numberCell(difficoltLevel)
+
+    gameContainer.innerHTML = "";
+    const numberCol = Math.sqrt(numberCellToAdd)
+    console.log(numberCol)
+
+    for (let index = 0; index < numberCellToAdd; index++) {
+        // const element = numberColToAdd[index];
+        //creazione dell'elemento
+        const cell = document.createElement("div")
+        cell.classList.add("cel-box-" + Math.round(numberCol))
+        gameContainer.append(cell)
+    }
+
+    console.log(difficoltLevel)
+})
+// funzione scelta livello
+function numberCell(difficoltLevel) {
+    let result
+
+    if (difficoltLevel === "easy") {
+        result = 49
+    }
+
+    else if (difficoltLevel === "medium") {
+        result = 81
+    }
+    else (
+        result = 100
+    )
+    return result
+
+}
