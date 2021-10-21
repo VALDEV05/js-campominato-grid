4550d613fb346f6e9301444ec64d17a6c27861e3
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-10-21 10:23:17 +0200

    Fixed the click problem
    Fixed the click problem, I changed the creation of the element, created it with .createElement ("div") and then added the classes as desired. For the click I have created an external function that manages the color, so that in the future it will be easier to access it.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-/*
+/* /*
 
 L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata,
     in cui ogni cella contiene un numero tra quelli compresi in un range:
@@ -10,51 +10,50 @@ L'utente indica un livello di difficoltà in base al quale viene generata una gr
 
 */
 
-const btnSubmit = document.getElementById("btn_start_game")
+const btnSubmit = document.getElementById("btn-start-game")
 const btnLevel = document.getElementById("levelup")
 const gameContainer = document.getElementById("boxgame")
 
-btnSubmit.addEventListener("click", function () {
+
+btnSubmit.addEventListener("click", function() {
     const difficoltLevel = btnLevel.value
-    const numberCellToAdd = numberCell(difficoltLevel)
-    let cell;
+    const numberCellToAdd = cellNumber(difficoltLevel)
 
     gameContainer.innerHTML = "";
-    const numberCol = Math.round(Math.sqrt(numberCellToAdd))
+    const numberCol = Math.sqrt(numberCellToAdd)
     console.log(numberCol)
 
     for (let index = 1; index <= numberCellToAdd; index++) {
-        /*console.log(index) */
-        //creazione dell'elemento
-        cell = `<div class="cell-box-${numberCol} cell" id="cell-${index}">${index}</div>`
-        gameContainer.innerHTML += cell
+        // const element = numberCellToAdd[x];
+        const cell = document.createElement("div")
+        cell.classList.add("cel-box-" + Math.round(numberCol))
+        gameContainer.append(cell)
+
+        cell.innerHTML += index
+        cell.addEventListener("click", cellOnClick)
 
 
-        const element = document.getElementById(`cell-${index}`)
-        console.log(element);
-        element.addEventListener('click', function () {
-            console.log(element);
-            console.log('ciao!');
-            this.style.backgroundColor = "red";
-        })
     }
 
-
+    console.log(difficoltLevel)
 })
-// funzione scelta livello
-function numberCell(difficoltLevel) {
-    let result
+
+//funzione che gestisce il numero delle celle in base al livello selezionato
+function cellNumber(difficoltLevel) {
+    let risultato
 
     if (difficoltLevel === "easy") {
-        result = 49
-    }
-
-    else if (difficoltLevel === "medium") {
-        result = 81
-    }
-    else (
-        result = 100
+        risultato = 100
+    } else if (difficoltLevel === "medium") {
+        risultato = 81
+    } else(
+        risultato = 49
     )
-    return result
 
+    return risultato
+
+}
+//funzione che gestisce l'interazione del click dell'utente
+function cellOnClick() {
+    this.classList.toggle("active")
 }
